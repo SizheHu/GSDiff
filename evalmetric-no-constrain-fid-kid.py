904882df227d2b662947648c1458fd09e851492a
Sizhe Hu <2024010072@mail.hfut.edu.cn>
2025-06-09 08:20:19 +0800

    Update evalmetric-no-constrain-fid-kid.py

--- evalmetric-no-constrain-fid-kid.py
+++ evalmetric-no-constrain-fid-kid.py
@@ -1,31 +1,33 @@
 import os
 import numpy as np
 
-# 初始化一个字典来存储所有的 FID 和 KID 值
+'''This is used to calculate the FID and KID (located in 'test_metrics.npy') calculated from the results of any inference under any constraints'''
+
+# Initialize a dictionary to store all FID and KID values
 metrics_dict = {}
 
-# 遍历当前目录下的所有文件夹
+# Traverse all folders in the current directory
 for folder in os.listdir('./T'):
     if os.path.isdir('./T/' + folder) and '-' in folder:
         if len(folder.split('-')) == 2 and folder.split('-')[0].isupper() and folder.split('-')[1].isdigit():
-            # 构建 test_metrics.npy 文件的完整路径
+            # Construct the full path to the test_metrics.npy file
             file_path = os.path.join('./T/' + folder, 'test_metrics.npy')
-            # 检查文件是否存在
+            # Check if a file exists
             if os.path.isfile(file_path):
-                # 加载 numpy 数组
+                # Loading numpy arrays
                 data = np.load(file_path, allow_pickle=True)[0]
-                # 获取 FID 和 KID 值
+                # Get FID and KID values
                 fid_value = float(data[1])
                 kid_value = float(data[2])
-                # 从文件夹名称中获取编号
+                # Get the number from the folder name
                 group, number = folder.split('-')
-                # 将数据添加到字典中
+                # Add data to the dictionary
                 if number not in metrics_dict:
                     metrics_dict[number] = {'FID': [], 'KID': []}
                 metrics_dict[number]['FID'].append(fid_value)
                 metrics_dict[number]['KID'].append(kid_value)
 
-# 输出每个编号的 FID 和 KID 平均值
+# Output the average FID and KID for each ID
 print(f"{'Number':<10}{'FID Average':<20}{'KID Average':<20}")
 for number, values in sorted(metrics_dict.items(), key=lambda x: int(x[0])):
     # print(values['FID'], type(values['FID']))
